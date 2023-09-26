782e581277bc0addd1d7eb9f129b6d6d92ea6674
ZTHLLY <zzz18011940139@gmail.com>
2023-09-26 22:58:23 +0800

    2023/9/26 si1v3r 前端登录页面展示

--- user-web/src/components/Footer/index.tsx
+++ user-web/src/components/Footer/index.tsx
@@ -1,41 +1,32 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
-import { useIntl } from 'umi';
-
 const Footer: React.FC = () => {
-  const intl = useIntl();
-  const defaultMessage = intl.formatMessage({
-    id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
-  });
-
+  const defaultMessage = 'si1v3r出品';
   const currentYear = new Date().getFullYear();
-
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'nothing here',
+          title: '笑死,没东西摆了',
+          href: '#',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/ZTHLLY',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'si1v3r\'s blog',
+          title: 'si1v3r\'s blog',
+          href: 'https://zthlly.github.io/',
           blankTarget: true,
         },
       ]}
     />
   );
 };
-
 export default Footer;
